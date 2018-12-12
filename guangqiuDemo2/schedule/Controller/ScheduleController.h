fee0c2de0ded75a594a22dcf42ef10689a52ddaa
刘兵 <liubing@liubingdeimac.local>
2018-12-12 10:58:31 +0800

    Initial Commit

---
+++ guangqiuDemo2/schedule/Controller/ScheduleController.h
@@ -0,0 +1,17 @@
+//
+//  ScheduleController.h
+//  guangqiuDemo2
+//
+//  Created by 刘兵 on 2018/12/11.
+//  Copyright © 2018年 刘兵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ScheduleController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ guangqiuDemo2Tests/guangqiuDemo2Tests.m
@@ -0,0 +1,37 @@
+//
+//  guangqiuDemo2Tests.m
+//  guangqiuDemo2Tests
+//
+//  Created by 刘兵 on 2018/12/11.
+//  Copyright © 2018年 刘兵. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface guangqiuDemo2Tests : XCTestCase
+
+@end
+
+@implementation guangqiuDemo2Tests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
